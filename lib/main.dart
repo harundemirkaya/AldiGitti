a94327136a9ea78c81d454dc986119b6972844d0
Harun Demirkaya <harundmrk@gmail.com>
2023-08-14 11:41:22 +0300

    Added Localizable for TR

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:aldigitti/Provider/DataProvider.dart';
 import 'package:aldigitti/Views/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +19,14 @@ class MyApp extends StatelessWidget {
       create: (context) => DataProvider(),
       child: MaterialApp(
         title: 'Aldı Gitii',
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('tr', 'TR'), // Türkçe
+        ],
         debugShowCheckedModeBanner: false,
         home: HomePage(),
         theme: ThemeData(
